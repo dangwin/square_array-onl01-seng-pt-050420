95ce7d467c763e339f5db452da5d99d8b755ca36
Dang Nguyen <danghwin@gmail.com>
2020-05-11 13:59:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
   new_numbers = []
-new_numbers.push(arrays.each do |x|
+new_numbers.push arrays.each do |x|
     new_numbers.push x * x
-  end)
+  end
+  
 new_numbers
+
 end
     
  
